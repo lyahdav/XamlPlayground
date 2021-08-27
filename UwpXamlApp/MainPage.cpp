502e5830137b8bd007c2b0aa1d73298ebb78fb68
Liron Yahdav <lyahdav@users.noreply.github.com>
2021-08-27 11:58:12 -0700

    Both apps run using shared UI

--- UwpXamlApp/MainPage.cpp
+++ UwpXamlApp/MainPage.cpp
@@ -1,6 +1,7 @@
 ﻿#include "pch.h"
 #include "MainPage.h"
 #include "MainPage.g.cpp"
+#include "Shared.h"
 
 using namespace winrt;
 using namespace Windows::UI::Xaml;
@@ -10,20 +11,8 @@ namespace winrt::UwpXamlApp::implementation
     MainPage::MainPage()
     {
         InitializeComponent();
-    }
 
-    int32_t MainPage::MyProperty()
-    {
-        throw hresult_not_implemented();
-    }
-
-    void MainPage::MyProperty(int32_t /* value */)
-    {
-        throw hresult_not_implemented();
-    }
-
-    void MainPage::ClickHandler(IInspectable const&, RoutedEventArgs const&)
-    {
-        myButton().Content(box_value(L"Clicked"));
+        StackPanel panel = Content().as<StackPanel>();
+        PopulateUI(panel);
     }
 }
