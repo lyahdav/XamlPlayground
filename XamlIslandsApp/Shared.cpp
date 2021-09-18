3da73dd4ea3c918ce8d69035c5ee30a08f75ebde
Liron Yahdav <lyahdav@users.noreply.github.com>
2021-09-17 17:49:26 -0700

    Example with size changing after load

--- XamlIslandsApp/Shared.cpp
+++ XamlIslandsApp/Shared.cpp
@@ -11,26 +11,31 @@ void PopulateUI(StackPanel xamlContainer) {
   
   ScrollViewer sv;
   TextBlock tb;
-
-  sv.Loaded([=](auto sender, auto &&...) {
-    auto sv = sender.as<ScrollViewer>();
-    sv.Height(sv.ActualHeight() - tb.ActualHeight());
+  StackPanel fz;
+  StackPanel fzc;
+  fzc.SizeChanged([=](auto&&...) {
+    sv.Height(fzc.ActualHeight());
     });
+
   sv.VerticalScrollMode(ScrollMode::Disabled);
   sv.VerticalScrollBarVisibility(ScrollBarVisibility::Hidden);
   xamlContainer.Children().Append(buttonWithText());
 
-  StackPanel fz;
   fz.Background(SolidColorBrush(Windows::UI::Colors::Red()));
   fz.TabFocusNavigation(xaml::Input::KeyboardNavigationMode::Once);
   fz.XYFocusKeyboardNavigation(xaml::Input::XYFocusKeyboardNavigationMode::Enabled);
 
-  fz.Children().Append(buttonWithText(L"Btn1"));
-  fz.Children().Append(buttonWithText(L"Btn2"));
+  fzc.Children().Append(buttonWithText(L"Btn1"));
+  fzc.Children().Append(buttonWithText(L"Btn2"));
+  fz.Children().Append(fzc);
   fz.Children().Append(tb);
 
   sv.Content(fz);
   xamlContainer.Children().Append(sv);
 
-  xamlContainer.Children().Append(buttonWithText());
+  auto btn = buttonWithText(L"Add btn to fz");
+  btn.Click([=](auto&&...) {
+    fzc.Children().Append(buttonWithText());
+    });
+  xamlContainer.Children().Append(btn);
 }
