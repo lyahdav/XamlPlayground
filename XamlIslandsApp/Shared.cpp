9b10dd8a8a5119547dce4b510700bb11cfc256e0
Liron Yahdav <lyahdav@users.noreply.github.com>
2022-01-06 15:22:43 -0800

    wip

--- XamlIslandsApp/Shared.cpp
+++ XamlIslandsApp/Shared.cpp
@@ -6,9 +6,27 @@ void PopulateUI(StackPanel xamlContainer) {
   Button btn;
   btn.Content(winrt::box_value(L"Button"));
   btn.Click([=](auto&&...) {
-    ContentDialog dlg;
-    dlg.Content(winrt::box_value(L"Content"));
+    ContentDialog dlg{};
+    TextBlock titleTextBlock;
+    TextBlock messageTextBlock;
+    titleTextBlock.Text(L"Title");
+    titleTextBlock.IsTextSelectionEnabled(true);
+    titleTextBlock.MaxLines(2);
+    titleTextBlock.TextWrapping(Xaml::TextWrapping::Wrap);
+    messageTextBlock.Text(L"Message Text");
+    messageTextBlock.IsTextSelectionEnabled(true);
+    messageTextBlock.TextWrapping(Xaml::TextWrapping::Wrap);
+    dlg.Title(titleTextBlock);
+    dlg.Content(messageTextBlock);
+    dlg.PrimaryButtonText(L"Primary Button");
+    dlg.SecondaryButtonText(L"Secondary Button");
     dlg.CloseButtonText(L"OK");
+
+    //ContentDialog dlg;
+    //dlg.Title(winrt::box_value(L"Title"));
+    //dlg.Content(winrt::box_value(L"Content"));
+    //dlg.CloseButtonText(L"OK");
+
     dlg.XamlRoot(xamlContainer.XamlRoot());
     dlg.ShowAsync();
     });
