501bb061fd51aeb03eaa8037f062a66400708afb
Liron Yahdav <lyahdav@users.noreply.github.com>
2022-12-20 16:52:50 -0800

    Simple Flyout

--- XamlIslandsApp/Shared.cpp
+++ XamlIslandsApp/Shared.cpp
@@ -5,41 +5,8 @@
 void PopulateUI(StackPanel xamlContainer) {
   xamlContainer.HorizontalAlignment(HorizontalAlignment::Left);
 
-  Button btn;
-  btn.Content(winrt::box_value(L"Open ContentDialog"));
-  btn.Click([=](auto&&...) {
-    ContentDialog dlg;
-    dlg.Content(winrt::box_value(L"Content"));
-    dlg.CloseButtonText(L"OK");
-    dlg.XamlRoot(xamlContainer.XamlRoot());
-    dlg.ShowAsync();
-    });
-
-
-  Button btn2;
-  btn2.Content(winrt::box_value(L"Button2"));
-
-  StackPanel p;
-  StackPanel p2;
-  p2.Width(100);
-  p2.Height(100);
-  p2.Background(SolidColorBrush(Colors::Red()));
-  p.Children().Append(btn);
-  p.Children().Append(p2);
-  p.Children().Append(btn2);
-
-  Button btnFlyout;
-  btnFlyout.Content(winrt::box_value(L"Open Flyout: ShouldConstrainToRootBounds=false"));
-  btnFlyout.Click([=](auto&&...) {
-		Flyout flyout;
-		flyout.ShouldConstrainToRootBounds(false);
-		flyout.Content(p);
-
-		flyout.ShowAt(btnFlyout);
-    });
-
   Button btnFlyout2;
-  btnFlyout2.Content(winrt::box_value(L"Open Flyout: ShouldConstrainToRootBounds=true"));
+  btnFlyout2.Content(winrt::box_value(L"Open Flyout"));
   btnFlyout2.Click([=](auto&&...) {
 		StackPanel p3;
 
@@ -58,6 +25,5 @@ void PopulateUI(StackPanel xamlContainer) {
 		flyout2.ShowAt(btnFlyout2);
     });
 
-  xamlContainer.Children().Append(btnFlyout);
   xamlContainer.Children().Append(btnFlyout2);
 }
